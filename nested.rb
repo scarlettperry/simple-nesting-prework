88526a79b4c3721795b5af59fb0a74c3254359ad
Scarlett Perry <si.perry15@gmail.com>
2018-07-03 18:21:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -63,7 +63,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP, C"]
 # }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -79,6 +78,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     programmer_hash[:yukihiro_matsumoto]=
+     {:known_for => "Ruby",:languages => ["LISP, C"] }
   programmer_hash
 end
 
